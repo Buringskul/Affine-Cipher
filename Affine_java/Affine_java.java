c3e79588a5949e6ed4eecb4e79b4a97123d71ea3
Simona Isakova <simonaisakova6@gmail.com>
2023-12-03 19:53:24 -0500

    Day 1

---
+++ Affine_java/Affine_java.java
@@ -0,0 +1,22 @@
+public class Affine_java {
+    static int a = 5;
+    static int b = 8;
+
+    static String encrypt(String message) {
+        String encrytped = "";
+        for (int i = 0; i < message.length(); i++) {
+
+        }
+        return encrytped;
+    }
+
+    static String decrypt(String cipher) {
+        String decrypted = "";
+        return decrypted;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
